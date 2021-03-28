5e4331048bc57332b71f5f386a79e6b98fe3d043
vipatakilembo <vkilembo@gmail.com>
2021-03-27 20:44:01 -0400

    Initial commit. Xcode files added

---
+++ MeetUp/MeetUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetUp
+//
+//  Created by Vipata Kilembo on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
