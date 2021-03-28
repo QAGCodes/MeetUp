ad966b6cecdb5f007664103a01aae6a6c9e8d33e
vipatakilembo <vkilembo@gmail.com>
2021-03-28 13:55:34 -0400

    Added the create/edit invite view controller along with its swift file

---
+++ MeetUp/MeetUp/EditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditViewController.swift
+//  MeetUp
+//
+//  Created by Vipata Kilembo on 3/28/21.
+//
+
+import UIKit
+
+class EditViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
