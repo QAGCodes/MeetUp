3e07ad05bb8f18fdebe2974c3895e182d3264dcd
QAGatPurdue <54636211+QAGatPurdue@users.noreply.github.com>
2021-04-03 14:34:03 -0400

    Done with designing the invite stream and the invite details screens

---
+++ MeetUp/MeetUp/InvitesStreamCell.swift
@@ -0,0 +1,23 @@
+//
+//  InvitesStreamCell.swift
+//  MeetUp
+//
+//  Created by csuser on 4/2/21.
+//
+
+import UIKit
+
+class InvitesStreamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
