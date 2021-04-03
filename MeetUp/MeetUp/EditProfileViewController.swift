d399d6e43769db1acc453aa8a344b27cb8686e23
ayushman <singh635@purdue.edu>
2021-04-03 17:45:03 -0400

    Created dummy edit profile page

---
+++ MeetUp/MeetUp/EditProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EditProfileViewController.swift
+//  MeetUp
+//
+//  Created by Ayushman Singh on 4/3/21.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var tempLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /* tempLabel.text = "gfmbgsjfvksgi shigsbkg skfg ksbjds bgkj dfbgjsbkfjdb f kjsv fkjsvf ksvf kjsvf jkzvfzkjfv sjkdfv ksdvfkjsdvf ksjvf ksfv skfvs kfvskjfv skjfv ksfvs jkdvsjdvfjks gfv skjfsjdvfsjf sfs" */
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
