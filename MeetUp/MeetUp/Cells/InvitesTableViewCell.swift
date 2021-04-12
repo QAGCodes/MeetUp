db14639c816094bcd187cbfb10da36c908eb2c59
QAGatPurdue <54636211+QAGatPurdue@users.noreply.github.com>
2021-04-12 02:42:54 -0400

    able to present some details of the invite, but some stuff needs more work to present

--- MeetUp/MeetUp/Cells/InvitesTableViewCell.swift
+++ MeetUp/MeetUp/Cells/InvitesTableViewCell.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class InvitesTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var inviterImage: UIImageView!
+    @IBOutlet weak var acceptButton: UIButton!
+    @IBOutlet weak var infoButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
